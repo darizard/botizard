79efe2139f67b5f0e93d2fdf706b921ef7db404b
Jon Strode <jstrode84@gmail.com>
2019-07-13 11:22:40 -0600

    add sub and vip checks to verifier

--- verifier.js
+++ verifier.js
@@ -6,6 +6,14 @@ module.exports.isBroadcaster = function(userContext) {
 	return(userHasBadge(userContext,"broadcaster"));
 }
 
+module.exports.isVIP = function(userContext) {
+	return(userHasBadge(userContext,"vip"));
+}
+
+module.exports.isSub = function(userContext) {
+	return(userHasBadge(userContext,"subscriber"));
+}
+
 function userHasBadge(userContext, badgeName) {
 	for(var b in userContext.badges) {
 		if(b == badgeName) return true;
