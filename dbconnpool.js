4481d58b620fddd2c67ca0f0a5fe49f1570c5443
darizard <u0348058@umail.utah.edu>
2019-07-30 15:06:56 -0600

    remove unnecessary comments

--- dbconnpool.js
+++ dbconnpool.js
@@ -28,24 +28,3 @@ class DBConnPool {
 }
 
 module.exports = DBConnPool;
-/*
-module.exports.connect = async function(db_name) {
-
-var pool = mysql.createPool(config[db_name]);
-
-	await pool.getConnection((err, connection) => {
-		if(err) {
-			console.error("An error occurred while connecting to the database");
-			console.error("Err code: " + err.code);
-		}
-
-		if(connection) {
-			connection.release();
-			console.log(`MySQL Connected (${db_name})`);
-		}
-		return;
-	});
-	pool.query = await util.promisify(pool.query);
-	return pool;
-}
-*/
